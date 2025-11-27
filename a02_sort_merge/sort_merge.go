af0abe1fa5b0873d1c8c0e90b38f3bd24cabec4b
TolkinSL <tolkinsl@yandex.ru>
2025-11-28 02:23:09 +0300

    algo03 Сортировка слиянием

--- a02_sort_merge/sort_merge.go
+++ a02_sort_merge/sort_merge.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func split(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := split(arr[:mid])
+	right := split(arr[mid:])
+
+	fmt.Println("left", left, "-", "right", right)
+	
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+	var l, r int
+
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			result = append(result, left[l])
+			l++
+		} else {
+			result = append(result, right[r])
+			r++
+		}
+	}
+
+	result = append(result, left[l:]...)
+	result = append(result, right[r:]...)
+
+	return result
+}
 
 func main() {
-	
+	arr := []int{7, 3, 6, 2, 8, 11, 15}
+	fmt.Println("Отсортированный срез", split(arr))
 }
